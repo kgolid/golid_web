a301eba8e722caf29753a1bbc7bbae8f0823a5e4
Kjetil Golid <kjetil.golid@gmail.com>
2016-07-30 03:03:47 +0200

    get browserify up and running

--- js/app.js
+++ js/app.js
@@ -1,89 +1,94 @@
-var house
+var p5 = require('p5');
 
-var max_dim = 400;
+var sketch = function(p) {
 
-function setup() {
-  var canv = createCanvas(500,500);
-  canv.parent("carousel");
-  background(10,200,155);
-  noFill();
-  stroke(51,51,51);
-  frameRate(2);
+  var house
+  var max_dim = 400;
 
-  house = new House(random(max_dim / 2, max_dim),random(max_dim / 2, max_dim));
-}
+  p.setup = function() {
+    p.createCanvas(500,500);
+    p.background(10,200,155);
+    p.noFill();
+    p.frameRate(2);
+    p.stroke(51);
 
-function draw() {
-  background(10,200,155);
-  house.display_house();
-  if(!house.done){
-    house.build_wall();
-  } else {
-    house = new House(random(max_dim / 2, max_dim),random(max_dim / 2, max_dim));
+    house = new House(p.random(max_dim / 2, max_dim), p.random(max_dim / 2, max_dim));
   }
-}
 
-function House(dimx, dimy) {
-  this.dim = new p5.Vector(dimx,dimy);
-  this.number_of_rooms = random(8,12);
-  this.outer_walls = new Room(this.dim.x, this.dim.y, (width-this.dim.x)/2, (height-this.dim.y)/2);
-  this.rooms = [];
-  this.rooms.push(this.outer_walls);
-  this.done = false;
-
-  this.build_wall = function() {
-    if (this.rooms.length < this.number_of_rooms){
-      var r = this.pop_biggest_room();
-      this.split_room(r);
+  p.draw = function() {
+    p.background(10,200,155);
+    house.display_house();
+    if(!house.done){
+      house.build_wall();
     } else {
-      this.done = true;
+      house = new House(p.random(max_dim / 2, max_dim), p.random(max_dim / 2, max_dim));
     }
   }
 
-  this.pop_biggest_room = function() {
-    var biggest = 0;
-    for(var i = 0; i < this.rooms.length; i++){
-      if(this.rooms[i].get_area() > this.rooms[biggest].get_area())
-        biggest = i;
+  function House(dimx, dimy) {
+    this.dim = new p5.Vector(dimx,dimy);
+    this.number_of_rooms = p.random(8,12);
+    this.outer_walls = new Room(this.dim.x, this.dim.y, (p.width-this.dim.x)/2, (p.height-this.dim.y)/2);
+    this.rooms = [];
+    this.rooms.push(this.outer_walls);
+    this.done = false;
+
+    this.build_wall = function() {
+      if (this.rooms.length < this.number_of_rooms){
+        var r = this.pop_biggest_room();
+        this.split_room(r);
+      } else {
+        this.done = true;
+      }
     }
-    return this.rooms.splice(biggest,1)[0];
-  }
 
-  this.split_room = function(r) {
-    if(r.dim.x > r.dim.y){
-      var f = random(r.dim.x/3) + r.dim.x/3;
-      var r1 = new Room(f, r.dim.y, r.pos.x, r.pos.y);
-      var r2 = new Room(r.dim.x - f, r.dim.y, r.pos.x + f, r.pos.y);
-      this.rooms.push(r1);
-      this.rooms.push(r2);
-    } else {
-      var f = random(r.dim.y/3) + r.dim.y/3;
-      var r1 = new Room(r.dim.x, f, r.pos.x, r.pos.y);
-      var r2 = new Room(r.dim.x, r.dim.y - f, r.pos.x, r.pos.y + f);
-      this.rooms.push(r1);
-      this.rooms.push(r2);
+    this.pop_biggest_room = function() {
+      var biggest = 0;
+      for(var i = 0; i < this.rooms.length; i++){
+        if(this.rooms[i].get_area() > this.rooms[biggest].get_area())
+          biggest = i;
+      }
+      return this.rooms.splice(biggest,1)[0];
+    }
+
+    this.split_room = function(r) {
+      if(r.dim.x > r.dim.y){
+        var f = p.random(r.dim.x/3) + r.dim.x/3;
+        var r1 = new Room(f, r.dim.y, r.pos.x, r.pos.y);
+        var r2 = new Room(r.dim.x - f, r.dim.y, r.pos.x + f, r.pos.y);
+        this.rooms.push(r1);
+        this.rooms.push(r2);
+      } else {
+        var f = p.random(r.dim.y/3) + r.dim.y/3;
+        var r1 = new Room(r.dim.x, f, r.pos.x, r.pos.y);
+        var r2 = new Room(r.dim.x, r.dim.y - f, r.pos.x, r.pos.y + f);
+        this.rooms.push(r1);
+        this.rooms.push(r2);
+      }
+    }
+
+    this.display_house = function() {
+      p.strokeWeight(8);
+      this.outer_walls.display_room();
+      p.strokeWeight(1);
+      for(a in this.rooms)
+        this.rooms[a].display_room();
     }
   }
 
-  this.display_house = function() {
-    strokeWeight(8);
-    this.outer_walls.display_room();
-    strokeWeight(1);
-    for(a in this.rooms)
-      this.rooms[a].display_room();
+
+  function Room(w, h, x, y) {
+    this.dim = new p5.Vector(w,h);
+    this.pos = new p5.Vector(x,y);
+
+    this.get_area = function(){
+      return this.dim.x * this.dim.y;
+    }
+
+    this.display_room = function(){
+      p.rect(this.pos.x, this.pos.y, this.dim.x, this.dim.y);
+    }
   }
-}
+};
 
-
-function Room(w, h, x, y) {
-  this.dim = new p5.Vector(w,h);
-  this.pos = new p5.Vector(x,y);
-
-  this.get_area = function(){
-    return this.dim.x * this.dim.y;
-  }
-
-  this.display_room = function(){
-    rect(this.pos.x, this.pos.y, this.dim.x, this.dim.y);
-  }
-}
+module.exports = new p5(sketch, "main");
